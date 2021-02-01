9595cc6a874badb6e962c0c513813094ac18fc80
anaclara-gf <anaclaragf@outlook.com>
2021-02-01 19:06:00 -0300

    last updates

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
-// const url = `https://www.wikiart.org/en/api/2/PaintingSearch?term=Abaporu&authSessionKey=ba5e4f69d898`;
+const firstPageButton = document.getElementById("button-input-nome");
+const nameInput = document.getElementById("input-nome");
+const pagInicial = document.getElementById("tela-inicial");
+const pagExposicao = document.getElementById("tela-exposicao");
+const formPagInicial = document.getElementById("form-pag-inicial");
+const headerNomeUser = document.getElementById("header-nome-usuario");
 
-// // const myList = document.querySelector('ul');
+firstPageButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    if(nameInput.value !== ""){
+        pagInicial.classList.add("none");
+        pagExposicao.classList.remove("none");
+        headerNomeUser.innerHTML = `<b>Olá, ${nameInput.value}!</b>`
+    } else {
+        const msgErro = document.createElement("p");
+        msgErro.innerText = "Você deve fornecer o seu nome para continuar!"
+        formPagInicial.appendChild(msgErro);
+    }
+})
 
-// fetch(url)
-//     .then(response => response.json())
-//     .then(json => {
-//         console.log(json)
-//         }
-//     )
-// .catch(console.error);
