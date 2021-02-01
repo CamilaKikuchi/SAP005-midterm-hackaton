fbe3e970cd309f1c4c6e0642158c654640185c00
anaclara-gf <anaclaragf@outlook.com>
2021-02-01 14:17:26 -0300

    initial css

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-const url = `https://www.wikiart.org/en/api/2/PaintingSearch?term=Abaporu&authSessionKey=ba5e4f69d898`;
+// const url = `https://www.wikiart.org/en/api/2/PaintingSearch?term=Abaporu&authSessionKey=ba5e4f69d898`;
 
-// const myList = document.querySelector('ul');
+// // const myList = document.querySelector('ul');
 
-fetch(url)
-    .then(response => response.json())
-    .then(json => {
-        console.log(json)
-        }
-    )
-.catch(console.error);
+// fetch(url)
+//     .then(response => response.json())
+//     .then(json => {
+//         console.log(json)
+//         }
+//     )
+// .catch(console.error);
