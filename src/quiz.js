b99318e37262a17fbb4198e310560f1dcd599eee
anaclara-gf <anaclaragf@outlook.com>
2021-02-02 18:41:46 -0300

    css + frases exposição

--- src/quiz.js
+++ src/quiz.js
@@ -6,22 +6,29 @@ document.getElementById('button-iniciar-quiz').addEventListener('click', functio
 //Página questão 1
 function primeiraPergunta() {
 	let divUm = document.getElementById('div1');
-	divUm.innerHTML = `<p>Pergunta 1 de 4:</p></br>
-    <p>Quem pintou a obra ‘Abaporu’?</p></br>
-    <img src="images/abaporu.jpg" alt="Abaporu" width=250 height=350>
-    <button id='bt1' value="errado">Pedro Américo</button>
-    <button id='bt2' value="errado">Beatriz Milhazes</button></br>
-    <button id='bt3' value="correto">Tarsila do Amaral</button>
-    <button id='bt4' value="errado">Candido Portinari</button></br>
-    <button class="btn-next" id="btnAgain" >Tentar novamente!</button></br>
-    <button class="btn-next" id="proximaPergunta" >Próxima</button>`;
+	divUm.innerHTML = `<p class='header-quiz'>Pergunta 1 de 4:</p>
+	<div class='pergunta'>
+		<p>Quem pintou a obra ‘Abaporu’?</p>
+	</div>
+	<img src="images/abaporu.jpg" alt="Abaporu" class='obra-quiz-img'>
+	<div class="btnsResposta">
+		<button id='bt1' class='bt1' value="errado">Pedro Américo</button>
+		<button id='bt2' class='bt2' value="errado">Beatriz Milhazes</button>
+		<button id='bt3' class='bt3' value="correto">Tarsila do Amaral</button>
+		<button id='bt4' class='bt4' value="errado">Candido Portinari</button>
+	</div>
+	<div class='buttons'>
+		<p class='acertoOuErro none' id="acertoOuErro"></p>
+		<button class="btn-tryagain none" id="btnAgain">Quer tentar novamente?</button>
+		<button class="btn-nextquestion" id="proximaPergunta">Próxima Pergunta</button>
+	</div>
+	`;
 
 	document.getElementById('proximaPergunta').disabled = true;
 	carregarBotoes();
 
 	document.getElementById('proximaPergunta').addEventListener('click', function() {
 		divUm = document.getElementById('div1').innerHTML = '';
-		document.getElementById('acertoOuErro').innerHTML = '';
 		segundaPergunta();
 	});
 }
@@ -29,22 +36,29 @@ function primeiraPergunta() {
 //Página questão 2
 function segundaPergunta() {
 	let divDois = document.getElementById('div2');
-	divDois.innerHTML = `<p>Pergunta 2 de 4:</p></br>
-    <img src="images/bumba-meu-boi.jpg" alt="Bumba meu Boi " width=250 height=350>
-    <p>O Bumba meu Boi é uma tradicional festa de qual estado?</p></br>
-    <button id='bt1' value="errado">Rio Grande do Norte</button>
-    <button id='bt2' value="correto">Maranhão</button></br>
-    <button id='bt3' value="errado">Amazonas</button>
-    <button id='bt4' value="errado">Bahia</button></br>
-    <button class="button" id="btnAgain" >Tentar novamente!</button></br>
-    <button class="button" id="proximaPergunta" >Próxima</button>`;
+	divDois.innerHTML = `<p class='header-quiz'>Pergunta 2 de 4:</p>
+	<div class='pergunta'>
+		<p>O Bumba meu Boi é uma tradicional festa de qual estado?</p>
+	</div>
+    <img src="images/bumba-meu-boi.jpg" alt="Bumba meu Boi" class='obra-quiz-img'>
+    <div class="btnsResposta">
+		<button id='bt1' class='bt1' value="errado">Rio Grande do Norte</button>
+		<button id='bt2' class='bt2' value="correto">Maranhão</button>
+		<button id='bt3' class='bt3' value="errado">Amazonas</button>
+		<button id='bt4' class='bt4' value="errado">Bahia</button>
+	</div>
+    <div class='buttons'>
+		<p class='acertoOuErro none' id="acertoOuErro"></p>
+		<button class="btn-tryagain none" id="btnAgain">Quer tentar novamente?</button>
+		<button class="btn-nextquestion" id="proximaPergunta">Próxima Pergunta</button>
+	</div>
+	`;
 
 	document.getElementById('proximaPergunta').disabled = true;
 	carregarBotoes();
 
 	document.getElementById('proximaPergunta').addEventListener('click', function() {
 		divDois = document.getElementById('div2').innerHTML = '';
-		document.getElementById('acertoOuErro').innerHTML = '';
 		terceiraPergunta();
 	});
 }
@@ -52,23 +66,29 @@ function segundaPergunta() {
 //Página questão 3
 function terceiraPergunta() {
 	let divTres = document.getElementById('div3');
-	divTres.innerHTML = `<p>Preparados?! Pergunta 3 de 4:</p></br>
-    <p>Qual artista plástico é conhecido por pintar bandeirinhas? </p></br>
-    <img src="images/grande-fachada-festiva.jpg" alt="Grande Fachada Festiva
-    " width=250 height=350>
-    <button id='bt1' value="errado">Aldemir Martins</button>
-    <button id='bt2' value="correto">Alfredo Volpi</button></br>
-    <button id='bt3' value="errado">Lasar Segall</button>
-    <button id='bt4' value="errado">Candido Portinari</button></br>
-    <button class="btn-next" id="btnAgain" >Tentar novamente!</button></br>
-    <button class="btn-next" id="proximaPergunta" >Próxima</button>`;
+	divTres.innerHTML = `<p class='header-quiz'>Pergunta 3 de 4:</p>
+	<div class='pergunta'>
+		<p>Qual artista plástico é conhecido por pintar bandeirinhas?</p>
+	</div>
+	<img src="images/grande-fachada-festiva.jpg" alt="Grande Fachada Festiva" class='obra-quiz-img'>
+	<div class="btnsResposta">
+		<button id='bt1' class='bt1' value="errado">Aldemir Martins</button>
+		<button id='bt2' class='bt2' value="correto">Alfredo Volpi</button>
+		<button id='bt3' class='bt3' value="errado">Lasar Segall</button>
+		<button id='bt4' class='bt4' value="errado">Candido Portinari</button>
+	</div>
+	<div class='buttons'>
+		<p class='acertoOuErro none' id="acertoOuErro"></p>
+		<button class="btn-tryagain none" id="btnAgain">Quer tentar novamente?</button>
+		<button class="btn-nextquestion" id="proximaPergunta">Próxima Pergunta</button>
+	</div>
+	`;
 
 	document.getElementById('proximaPergunta').disabled = true;
 	carregarBotoes();
 
 	document.getElementById('proximaPergunta').addEventListener('click', function() {
 		divTres = document.getElementById('div3').innerHTML = '';
-		document.getElementById('acertoOuErro').innerHTML = '';
 		quartaPergunta();
 	});
 }
@@ -76,36 +96,35 @@ function terceiraPergunta() {
 //Página questão 4
 function quartaPergunta() {
 	let divQuatro = document.getElementById('div4');
-	divQuatro.innerHTML = `<p>Pergunta 4 de 4:</p></br>
-    <p>Nas margens de qual rio, Dom Pedro I declarou a independência do Brasil?</p></br>
-    <img src="images/independencia.jpg" alt="Independência ou Morte!" width=250 height=350>
-    <button id='bt1' value="correto">Rio Ipiranga</button>
-    <button id='bt2' value="errado">Rio Tiete</button></br>
-    <button id='bt3' value="errado">Rio Grande do Sul</button>
-    <button id='bt4' value="errado">Rio Pinheiros</button></br>
-    <button class="button" id="btnAgain" >Tentar novamente!</button></br>
-    <button class="button" id="proximaPergunta" >Finalizar quiz!</button>`;
+	divQuatro.innerHTML = `<p class='header-quiz'>Pergunta 4 de 4:</p>
+	<div class='pergunta'>
+		<p>Nas margens de qual rio,<br> Dom Pedro I declarou a independência?</p>
+	</div>
+	<img src="images/independencia.jpg" alt="Independência ou Morte!" class='obra-quiz-img'>
+	<div class="btnsResposta">
+		<button id='bt1' class='bt1' value="correto">Rio Ipiranga</button>
+		<button id='bt2' class='bt2' value="errado">Rio Tiete</button>
+		<button id='bt3' class='bt3' value="errado">Rio Grande do Sul</button>
+		<button id='bt4' class='bt4' value="errado">Rio Pinheiros</button>
+	</div>
+    <div class='buttons'>
+		<p class='acertoOuErro none' id="acertoOuErro"></p>
+		<button class="btn-tryagain none" id="btnAgain">Quer tentar novamente?</button>
+		<button class="btn-nextquestion btn-lastquestion" id="proximaPergunta">Finalizar quiz!</button>
+	</div>
+	`;
 
 	document.getElementById('proximaPergunta').disabled = true;
 	carregarBotoes();
 
 	document.getElementById('proximaPergunta').addEventListener('click', function() {
 		divQuatro = document.getElementById('div4').innerHTML = '';
-		document.getElementById('acertoOuErro').innerHTML = '';
-		paginaFinalDoQuiz();
+		const pagQuiz = document.getElementById("tela-quiz");
+		const pagBilheteDourado = document.getElementById("tela-bilhete-dourado");
+		pagQuiz.classList.add('none');
+		pagBilheteDourado.classList.remove('none');
+		window.scrollTo(0,0);
 	});
-}
-
-// Página final do quiz
-function paginaFinalDoQuiz() {
-	let divCinco = document.getElementById('div5');
-	divCinco.innerHTML = ` 
-	<p class="texto-bilhete-dourado"><b>VOCÊ CONCLUIU A TOUR!</b></p>
-	<p class="texto-bilhete-dourado">Printe seu segundo bilhete e compartilhe a experiência com seus amigos!</p>
-	<p class="texto-bilhete-dourado"><i>Não esqueça de convidá-los para jogar também!</i></p>
-	<img src="./images/bilhete-dourado.png" alt="bilhete-dourado" class="bilhete-img" />
-	<p class="bilhete-dourado-volte-sempre"><b>Volte Sempre!</b></p>
-	 `;
 }
 
 //Função carregar botões da página
@@ -114,27 +133,39 @@ function carregarBotoes() {
 
 	document.getElementById('btnAgain').disabled = true;
 
-	document.getElementById('bt1').addEventListener('click', function() {
+	document.getElementById('bt1').addEventListener('click', function(event) {
+		event.target.classList.add("button-orange");
 		resposta = document.getElementById('bt1').value;
 		verificaResposta(resposta);
 	});
 
-	document.getElementById('bt2').addEventListener('click', function() {
+	document.getElementById('bt2').addEventListener('click', function(event) {
+		event.target.classList.add("button-orange");
 		resposta = document.getElementById('bt2').value;
 		verificaResposta(resposta);
 	});
 
-	document.getElementById('bt3').addEventListener('click', function() {
+	document.getElementById('bt3').addEventListener('click', function(event) {
+		event.target.classList.add("button-orange");
 		resposta = document.getElementById('bt3').value;
 		verificaResposta(resposta);
 	});
 
-	document.getElementById('bt4').addEventListener('click', function() {
+	document.getElementById('bt4').addEventListener('click', function(event) {
+		event.target.classList.add("button-orange");
 		resposta = document.getElementById('bt4').value;
 		verificaResposta(resposta);
 	});
 
 	document.getElementById('btnAgain').addEventListener('click', function() {
+		const bt1 = document.getElementById('bt1');
+		const bt2 = document.getElementById('bt2');
+		const bt3 = document.getElementById('bt3');
+		const bt4 = document.getElementById('bt4');
+		bt1.classList.remove("button-orange");
+		bt2.classList.remove("button-orange");
+		bt3.classList.remove("button-orange");
+		bt4.classList.remove("button-orange");
 		habilitaBotoes();
 	});
 }
@@ -151,19 +182,28 @@ function habilitaBotoes() {
 //Função que verifica se resposta está correta ou não
 function verificaResposta(resposta) {
 	if (resposta == 'correto') {
-		document.getElementById('acertoOuErro').innerHTML = 'Muito bem você acertou!';
+		const rightQuestion = document.getElementById('acertoOuErro');
+		rightQuestion.classList.remove('none');
+		rightQuestion.classList.add('green-text')
+		rightQuestion.innerHTML = '&#10038; Muito bem você acertou! &#10038;';
 		document.getElementById('proximaPergunta').disabled = false;
 		document.getElementById('bt1').disabled = true;
 		document.getElementById('bt2').disabled = true;
 		document.getElementById('bt3').disabled = true;
 		document.getElementById('bt4').disabled = true;
-		document.getElementById('btnAgain').disabled = true;
 	} else {
-		document.getElementById('acertoOuErro').innerHTML = 'Humm, a resposta ainda não é essa!';
+		const notThisYet = document.getElementById('acertoOuErro');
+		notThisYet.classList.remove('none');
+		notThisYet.innerHTML = 'Humm, a resposta ainda não é essa!';
 		document.getElementById('bt1').disabled = true;
 		document.getElementById('bt2').disabled = true;
 		document.getElementById('bt3').disabled = true;
 		document.getElementById('bt4').disabled = true;
-		document.getElementById('btnAgain').disabled = false;
+		document.getElementById('proximaPergunta').disabled = false;
+		const btnAgain = document.getElementById('btnAgain');
+		btnAgain.classList.remove('none');
+		btnAgain.disabled = false;
+
+		btnAgain.addEventListener('click', () => btnAgain.classList.add('none'));
 	}
 }
