dd7575c9744198488d828675b9d74ba06b37046a
karinefrontelli <karinefrontellidossantos@gmail.com>
2021-02-02 11:04:14 -0300

    botao iniciar quiz e bilhete dourado

--- quiz.js
+++ quiz.js
@@ -1,5 +1,5 @@
 // Página inicial do quiz.
-document.getElementById('iniciarQuiz').addEventListener('click', function() {
+document.getElementById('button-iniciar-quiz').addEventListener('click', function() {
 	primeiraPergunta();
 });
 
@@ -8,7 +8,7 @@ function primeiraPergunta() {
 	let divUm = document.getElementById('div1');
 	divUm.innerHTML = `<p>Pergunta 1 de 4:</p></br>
     <p>Quem pintou a obra ‘Abaporu’?</p></br>
-    <img src="images/abaporu.jpg" alt="oi" width=250 height=350>
+    <img src="images/abaporu.jpg" alt="Abaporu" width=250 height=350>
     <button id='bt1' value="errado">Pedro Américo</button>
     <button id='bt2' value="errado">Beatriz Milhazes</button></br>
     <button id='bt3' value="correto">Tarsila do Amaral</button>
@@ -30,7 +30,7 @@ function primeiraPergunta() {
 function segundaPergunta() {
 	let divDois = document.getElementById('div2');
 	divDois.innerHTML = `<p>Pergunta 2 de 4:</p></br>
-    <img src="images/bumba-meu-boi.jpg" alt="Pintura da obra " width=250 height=350>
+    <img src="images/bumba-meu-boi.jpg" alt="Bumba meu Boi " width=250 height=350>
     <p>O Bumba meu Boi é uma tradicional festa de qual estado?</p></br>
     <button id='bt1' value="errado">Rio Grande do Norte</button>
     <button id='bt2' value="correto">Maranhão</button></br>
@@ -54,7 +54,8 @@ function terceiraPergunta() {
 	let divTres = document.getElementById('div3');
 	divTres.innerHTML = `<p>Preparados?! Pergunta 3 de 4:</p></br>
     <p>Qual artista plástico é conhecido por pintar bandeirinhas? </p></br>
-    <img src="images/grande-fachada-festiva.jpg" alt="Pintura da obra " width=250 height=350>
+    <img src="images/grande-fachada-festiva.jpg" alt="Grande Fachada Festiva
+    " width=250 height=350>
     <button id='bt1' value="errado">Aldemir Martins</button>
     <button id='bt2' value="correto">Alfredo Volpi</button></br>
     <button id='bt3' value="errado">Lasar Segall</button>
@@ -77,13 +78,13 @@ function quartaPergunta() {
 	let divQuatro = document.getElementById('div4');
 	divQuatro.innerHTML = `<p>Pergunta 4 de 4:</p></br>
     <p>Nas margens de qual rio, Dom Pedro I declarou a independência do Brasil?</p></br>
-    <img src="images/independencia.jpg" alt="Pintura da obra " width=250 height=350>
+    <img src="images/independencia.jpg" alt="Independência ou Morte!" width=250 height=350>
     <button id='bt1' value="correto">Rio Ipiranga</button>
     <button id='bt2' value="errado">Rio Tiete</button></br>
-    <button id='bt3' value="errado">Rio Ipiranga</button>
+    <button id='bt3' value="errado">Rio Grande do Sul</button>
     <button id='bt4' value="errado">Rio Pinheiros</button></br>
     <button class="button" id="btnAgain" >Tentar novamente!</button></br>
-    <button class="button" id="proximaPergunta" >Finalizar quiz!.</button>`;
+    <button class="button" id="proximaPergunta" >Finalizar quiz!</button>`;
 
 	document.getElementById('proximaPergunta').disabled = true;
 	carregarBotoes();
@@ -91,15 +92,21 @@ function quartaPergunta() {
 	document.getElementById('proximaPergunta').addEventListener('click', function() {
 		divQuatro = document.getElementById('div4').innerHTML = '';
 		document.getElementById('acertoOuErro').innerHTML = '';
-		//paginaFinalDoQuiz();
+		paginaFinalDoQuiz();
 	});
 }
 
-//Página final do quiz
-// function paginaFinalDoQuiz() {
-// 	let divCinco = document.getElementById('div5');
-// 	divCinco.innerHTML = ` <img src="images/bilhete-dourado.png" alt="Yeahhh" width=320 height=620>`;
-// }
+// Página final do quiz
+function paginaFinalDoQuiz() {
+	let divCinco = document.getElementById('div5');
+	divCinco.innerHTML = ` 
+	<p class="texto-bilhete-dourado"><b>VOCÊ CONCLUIU A TOUR!</b></p>
+	<p class="texto-bilhete-dourado">Printe seu segundo bilhete e compartilhe a experiência com seus amigos!</p>
+	<p class="texto-bilhete-dourado"><i>Não esqueça de convidá-los para jogar também!</i></p>
+	<img src="./images/bilhete-dourado.png" alt="bilhete-dourado" class="bilhete-img" />
+	<p class="bilhete-dourado-volte-sempre"><b>Volte Sempre!</b></p>
+	 `;
+}
 
 //Função carregar botões da página
 function carregarBotoes() {
