4409d619da59c2bfcec58cace0e55150ad7b50d4
anaclara-gf <anaclaragf@outlook.com>
2021-02-02 18:41:46 -0300

    css + frases exposição

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const obrasEscolhidas = [];
 const data = "https://thingproxy.freeboard.io/fetch/";
 const urls = [
@@ -9,11 +10,49 @@ const urls = [
     "https://www.wikiart.org/en/api/2/PaintingSearch?term=aldemir-martins-Bumba-meu-Boi",
     "https://www.wikiart.org/en/api/2/PaintingSearch?term=morro-vermelho-lasar-segall",
     "https://www.wikiart.org/en/api/2/PaintingSearch?term=girl-with-the-gas-mask",
-    "https://www.wikiart.org/en/api/2/PaintingSearch?term=volpi-grande-fachada-festiva"
+    "https://www.wikiart.org/en/api/2/PaintingSearch?term=volpi-grande-fachada-festiva",
+    "https://www.wikiart.org/en/api/2/PaintingSearch?term=meu-limao"
 ]
 
 let obrasPaginated = "";
 const listaDeObras = document.getElementById("lista-obras");
+
+function frase(nomeDaObra) {
+    let frase;
+    switch(nomeDaObra) {
+        case "Abaporu":
+            frase = "Onde será que esse homem compra sapatos?";
+            break;
+        case "Morro da favela":
+            frase = "Esse lugar se parece com seu bairro?";
+            break;
+        case "Futebol":
+            frase = "Você já jogou futebol na terra?";
+            break;
+        case "Independence or Death! (The Ipiranga Shout)":
+            frase = "Você sabia que esse quadro retrata o momento da Independência do Brasil? (Boatos que tinha meia dúzia de pessoas montadas em burrinhos)";
+            break;
+        case "A Cuca":
+            frase = "Que bicho é esse?";
+            break;
+        case "Bumba meu Boi":
+            frase = "Você conhece o Bumba meu Boi? É um folguedo do Maranhão e Patrimônio Cultural Imaterial da Humanidade.";
+            break;
+        case "Morro Vermelho":
+            frase = "Esse aqui dá calor só de olhar, não é não?";
+            break;
+        case "Girl With The Gas Mask":
+            frase = "Como você acha que seremos lembrados no futuro?";
+            break;
+        case "Grande fachada festiva":
+            frase = "Tem um artista conhecido só por pintar bandeirinhas! O que elas te lembram?";
+            break;
+        case "Meu Limão":
+            frase = "Esse quadro aqui se chama “Meu limão”. Sim, eu também não entendi nada.";
+            break;
+    }
+    return frase;
+}
 
 const promises = urls.map(url => fetch(data+url).then(response => response.json()));
 Promise.all(promises).then(allData => allData.map(obra => {
@@ -24,8 +63,9 @@ Promise.all(promises).then(allData => allData.map(obra => {
     obrasPaginated += `
         <li>
             <img src="${dadosDaObra.image}" alt="${dadosDaObra.artistUrl}-${dadosDaObra.title}" class="obra-img" />
-            <p>${dadosDaObra.title} (${dadosDaObra.completitionYear})</p>
-            <p>${dadosDaObra.artistName}</p>
+            <p class="frase-obra"><i><b>${frase(dadosDaObra.title)}</i></b></p>
+            <p class="dados-obra">${dadosDaObra.title} (${dadosDaObra.completitionYear})</p>
+            <p class="dados-obra">${dadosDaObra.artistName}</p>
         </li>
     `
 
